920fc107cdec543ea59e92dc591f3fc9ee2d5016
MrRobot <leco_lequitoo@hotmail.com>
2019-11-05 22:51:38 -0300

    Start commit!

--- app/src/main/java/com/example/testeapp/NumeroExp.kt
+++ app/src/main/java/com/example/testeapp/NumeroExp.kt
@@ -12,22 +12,16 @@ fun main (args: Array<String>) {
     var nome   = readLine()
 
 
-    nome = nome?.replace(" ", "")
+    nome = nome?.replace(" ", "")  // tirando espaçamentos
 
 
-
-
-        var (numero) = nome!!.partition { it.isDigit() }
-        var (letra) = nome!!.partition { it.isLetter() }
-        var (sinais) = nome!!.partition { !it.isLetterOrDigit() }
+        var (numero) = nome!!.partition { it.isDigit() }  // verificando numeros
+        var (letra) = nome!!.partition { it.isLetter() }  // verificando letras
+        var (sinais) = nome!!.partition { !it.isLetterOrDigit() }  // verificando caracteres
 
         println("São Numeros : " + numero)
         println("São Letras  : " + letra)
         println("São Sinais : " + sinais)
-
-
-
-
 
 
 }
