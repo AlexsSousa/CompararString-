9ac9ea7d47595823a99a94f54961952dc45a1ccb
MrRobot <leco_lequitoo@hotmail.com>
2019-11-05 22:43:52 -0300

    Start commit!

--- app/src/main/java/com/example/testeapp/NumeroExp.kt
+++ app/src/main/java/com/example/testeapp/NumeroExp.kt
@@ -7,17 +7,20 @@ import java.text.CharacterIterator
 
 fun main (args: Array<String>) {
 
-    var nome   = "A l e x 8 a l o 8 6 3 d + - /"
+    println("Escreva algo : ")
+    var textoInput = readLine()
+
+    var nome   = textoInput
 
 
-    nome = nome.replace(" ", "")
+    nome = nome?.replace(" ", "")
 
 
 
 
-        val (numero) = nome.partition { it.isDigit() }
-        val (letra) = nome.partition { it.isLetter() }
-        val (sinais) = nome.partition { !it.isLetterOrDigit() }
+        var (numero) = nome!!.partition { it.isDigit() }
+        var (letra) = nome!!.partition { it.isLetter() }
+        var (sinais) = nome!!.partition { !it.isLetterOrDigit() }
 
         println("São Numeros : " + numero)
         println("São Letras  : " + letra)
